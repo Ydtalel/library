7124fdb7503cfb7139578e39a675dccf28623fb3
Yuriy Mironenko <ydtalel@gmail.com>
2025-01-16 19:38:51 +0300

    change dotenv with pydantic_settings

--- app/config.py
+++ app/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://user:password@localhost:5432"
+    DATABASE_URL: str
     JWT_SECRET_KEY: str = "your_jwt_secret"
     JWT_ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
